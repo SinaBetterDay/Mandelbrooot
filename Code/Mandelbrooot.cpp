1239aea0db7d714acc8fd716da3891ddd1f342c2
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2022-09-30 23:41:11 -0700

    Update Mandelbrooot.cpp

--- Code/Mandelbrooot.cpp
+++ Code/Mandelbrooot.cpp
@@ -1,46 +1,133 @@
 #include <iostream>
-#include <SFML/Graphics>
+#include <vector>     
+#include <complex> 
+#include <iomanip>
+#include <SFML/Graphics.hpp>
+#include "ComplexPlane.h"
+
+
 
 using namespace std;
 using namespace sf;
 
-const int width = 1920;
-const int height = 1080;
-
-
-void gen_mandelbrot(VertexArray& va, int shift_x, int shift_y, int max_iter, float zoom)
+int main()
 {
-  for (int i = 0; i < height; i++)
-  {
-    for (int j = 0; j < width; j++)
-    {
-      long double x = ((long double)j - shift_x) / zoom;
-      long double y = ((long double)u - shift_y) / zoom;
-      
-      complex<long double> c(x,y);
-      int n = 0; //number of iterations, we have to keep track of this variable
-      
-      complex<long double> z(0.0, 0.0);
-      
-      for (int k = 0; k < max_iter; k++)
-      {
-        complex<long double> z2(0.0, 0.0);
-        z2.real(real(z) * real(z) - imag(z) * imag(z));
-        z2.imag(2 * real(z) * imag(z));
-        
-        z2.real(real(z2) + real(c)); // add c to z
-        z2.imag(imag(z2) + imag(c));
-        
-        n++;
-        
-        // to break of this loop, we either reach teh max_iter
-        // or the absolute(z+z) > 2
+    Defineset mandle(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height);
 
-int main() {
-  String title = "Mandelbrot_Plotter";
-  RenderWindow w(VideoMode (width, height), titles);
-  
-  
-  
-  return 0;
+
+    // Create a window with the same pixel depth as the desktop
+
+    sf::RenderWindow window;
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    window.create(sf::VideoMode(mandle.returnxpixels(), mandle.returnypixels()), "SFML window");
+    window.setFramerateLimit(60);
+    // run the program as long as the window is open
+
+    sf::Text textcalc;
+    sf::Text text;
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf"))
+    {
+        // error...
+    }
+
+
+    auto &clock = sf::Clock();
+
+    text = sf::Text("", font, 24);
+    text.setFillColor(sf::Color::White);
+    text.setOutlineThickness(2);
+    text.setOutlineColor(sf::Color::Black);
+    text.setPosition({ 10, 5 });
+    textcalc = sf::Text("", font, 24);
+
+    textcalc.setFillColor(sf::Color::White);
+    textcalc.setOutlineThickness(2);
+    textcalc.setOutlineColor(sf::Color::Black);
+    textcalc.setPosition({ 500, 500 });
+
+    
+
+    
+
+    while (window.isOpen())
+    {
+        
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+            {
+                mandle.leftkey();
+            }
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+            {
+                mandle.rightkey();
+            }
+            if (Keyboard::isKeyPressed(Keyboard::A)) {
+                mandle.akey();
+            }
+            if (Keyboard::isKeyPressed(Keyboard::W)) {
+                mandle.wkey();
+            }
+            if (Keyboard::isKeyPressed(Keyboard::S)) {
+                mandle.skey();
+            }
+            if (Keyboard::isKeyPressed(Keyboard::D)) {
+                mandle.dkey();
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Add)) {
+                mandle.pluskey();
+
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Subtract)) {
+                mandle.minuskey();
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+                window.close();
+            };
+
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.display();
+        window.clear();
+        if (mandle.pullbool() == true) {
+            window.draw(text);
+            window.draw(mandle.recoverarray());
+        }
+
+        if (mandle.pullbool() == true) {
+            auto text_builder = std::ostringstream();
+            text_builder << setw(4) << "Calculating";
+            textcalc.setString(text_builder.str());
+            window.draw(textcalc);
+            window.display();
+
+
+        }
+
+
+        if (mandle.pullbool() == true) {
+           
+            mandle.calcuatevetex();
+            mandle.setboolfalse();
+            window.display();
+            
+        }
+       
+        auto text_builder = std::ostringstream();        
+        text_builder << setw(4) << int(1 / clock.restart().asSeconds()) << " fps\n";
+        text_builder << setw(4) << mandle.pulliter() << " iters\n";
+        text_builder << setprecision(1) << std::scientific << (complex<float>)mandle.pulliter() / mandle.pullcomplexvalue() << '\n';
+        text_builder << "Right Click:zoom" << endl << "Left Click:Unzoom" << endl << " + - for Iteration" << endl << " A W S D Movement";
+        
+        text.setString(text_builder.str());
+        window.draw(mandle.recoverarray());
+        window.draw(text);
+
+
+    }
 }
