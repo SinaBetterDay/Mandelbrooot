db6ebaaa0ce6072ece81467a673a4591a1b7ca4e
Sina Vaghefi <55566349+SinaBetterDay@users.noreply.github.com>
2022-10-01 14:44:18 -0700

    Update ComplexPlane.cpp

--- Code/ComplexPlane.cpp
+++ Code/ComplexPlane.cpp
@@ -1,130 +1,153 @@
-#include <iostream>
-#include <vector>     
-#include <complex> 
-#include <iomanip>
-#include <SFML/Graphics.hpp>
 #include "ComplexPlane.h"
 
 
+ComplexPlane::ComplexPlane(float aspectRatio) {
 
-
-Defineset::Defineset(unsigned int _xpixels, unsigned int _ypixels)
-{
-    xpixels = _xpixels;
-    ypixels = _ypixels;
-    points.setPrimitiveType(sf::Points);
-    points.resize(ypixels * xpixels);
+    m_aspectratio = aspectRatio;
+    m_view.setSize(BASE_WIDTH, -BASE_HEIGHT * m_aspectratio);
+    m_view.setCenter(0.0, 0.0);
+    m_zoomcount = 0;
 
 }
-void Defineset::calcuatevetex()
-{
-    ///sf::VertexArray points(sf::Points, xpixels + 1 + ypixels * xpixels);
-        
 
-    for (int j = 0; j < xpixels; j++)
+void ComplexPlane::zoomIn() {
+
+    m_zoomcount++;
+
+    float x = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomcount));
+    float y = BASE_HEIGHT * m_aspectratio * (pow(BASE_ZOOM, m_zoomcount));
+
+    m_view.setSize(x, y);
+
+}
+
+void ComplexPlane::zoomOut() {
+
+    m_zoomcount--;
+
+    float x = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomcount));
+    float y = BASE_HEIGHT * m_aspectratio * (pow(BASE_ZOOM, m_zoomcount));
+
+    m_view.setSize(x, y);
+
+}
+
+void ComplexPlane::setCenter(Vector2f coord) 
+{
+    m_view.setCenter(coord);
+}
+
+View ComplexPlane::getView()
+{
+    return m_view;
+}
+
+void ComplexPlane::setMouseLocation(Vector2f coord) 
+{
+    m_mouselocation = coord;
+}
+
+void ComplexPlane::loadText(Text& text) {
+
+    text.setString("mandelbrooooooot");
+    text.setCharacterSize(20);
+    text.setFillColor(Color::Magenta);
+    text.setPosition(500, 500);
+
+
+}
+
+size_t ComplexPlane::countIterations(Vector2f coord) {
+
+	complex<double> c(coord.x, coord.y);
+	complex<double> z(0, 0);
+	size_t iter = 0;
+
+	while (abs(z) < 2 && iter < MAX_ITER)
+	{
+
+		z = z * z + c;
+		iter++;
+	}
+	return iter;
+
+}
+
+//edit function parameters
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b) {
+
+    if (count == MAX_ITER)
     {
-        for (int i = 0; i < ypixels; i++)
-        {
+        r = 0;
+        g = 0;
+        b = 0;
 
-            point.x = j;
-            point.y = i;
-            convertxytocomplex();
-            FindIterationsFromC();
-            ConvertIterToColor();
-            points[j + (i * xpixels)].position = point;
-            points[j + (i * xpixels)].color = color;
-
-        }
     }
-    vArray = points;
-}
 
+    else if (count > 57) {
+        r = 255;
+        g = 255 * (0);
+        b = 255 * (0);
 
-
-sf::VertexArray& Defineset::recoverarray() {
-    return vArray;
-}
-void Defineset::ConvertIterToColor() {
-
-    int t = iter;
-    int r = 9 * (1 - t) * pow(t, 3);
-    int g = 15 * pow((1 - t), 2) * pow(t, 3);
-    int b = 8.5 * pow((1 - t), 3) * t;
-    color.r = r;
-    color.g = g;
-    color.b = b;
-
-}
-void Defineset::FindIterationsFromC()
-{
-    std::complex<float> z;
-    for (int i = 0; i < MAX_ITER; i++) {
-        z = z * z + c;
-        if (abs(z) >= 2) {
-            iter = i;
-            return;
-
-
-        }
-        iter = 0;
     }
-}
 
-void Defineset::convertxytocomplex() {
-    c = { (point.x - xpixels / 2) * BASE_ZOOM + xset,
-        (point.y - xpixels / 2) * BASE_ZOOM + yset };
+    else if (count > 49) {
+        r = 255;
+        g = 255 / 2;
+        b = 255 * (0);
+    }
+
+    else if (count > 41)
+    {
+        r = 255 * (0.75);
+        g = 255;
+        b = 255 * (0);
+
+    }
+
+    else if (count > 33)
+    {
+        r = 255 * (0.5);
+        g = 255;
+        b = 255 * (0);
+
+    }
+
+    else if (count > 25)
+    {
+        r = 255 * (0);
+        g = 255;
+        b = 255 * (0.5);
+
+    }
+
+    else if (count > 17)
+    {
+        r = 255 * (0);
+        g = 255 * (0.25);
+        b = 255;
+
+    }
+
+    else if (count > 9)
+    {
+        r = 255 * (0.33);
+        g = 255 * (0);
+        b = 255;
+
+    }
+
+    else
+    {
+        r = 255;
+        g = 255 * (0);
+        b = 255;
+    }
 
 
 
-}
-std::complex<float> Defineset::pullcomplexvalue() {
-    return c;
-}
-int Defineset::returnxpixels() {
-    return xpixels;
-}
-int Defineset::returnypixels() {
-    return ypixels;
-}
-void Defineset::wkey() {
-    yset -= 40 * BASE_ZOOM;
-    calculate = true;
-}
-void Defineset::akey() {
-    xset -= 40 * BASE_ZOOM;
-    calculate = true;
-}
-void Defineset::skey() {
-    yset += 40 * BASE_ZOOM;
-    calculate = true;
-}
-void Defineset::dkey() {
-    xset += 40 * BASE_ZOOM;
-    calculate = true;
-}
-void Defineset::leftkey() {
-    BASE_ZOOM /= 0.9;
-    calculate = true;
-}
-void Defineset::rightkey() {
-    BASE_ZOOM *= 0.9;
-    calculate = true;
-}
-void Defineset::pluskey() {
-    MAX_ITER = MAX_ITER + 10;
-    calculate = true;
-}
-void Defineset::minuskey() {
-    MAX_ITER = MAX_ITER - 10;
-    calculate = true;
-}
-bool Defineset::pullbool() {
-    return calculate;
-}
-void Defineset::setboolfalse() {
-    calculate = false;
-    
-}
-int Defineset::pulliter() {
-    return MAX_ITER;
+
+
+
+
 }
